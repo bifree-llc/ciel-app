a46450f0b5ec1a4b3a810621d16a59ac3ba6a1ab
bifree <bifree2bfree@gmail.com>
2023-09-25 04:10:01 +0900

    terraform fmt

--- 01_infra/network.tf
+++ 01_infra/network.tf
@@ -90,12 +90,12 @@ resource "aws_route_table" "public_rt" {
 
 resource "aws_route_table_association" "public_rt_1a" {
   route_table_id = aws_route_table.public_rt.id
-  subnet_id = aws_subnet.public_subnet_1a.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
 }
 
 resource "aws_route_table_association" "public_rt_1c" {
   route_table_id = aws_route_table.public_rt.id
-  subnet_id = aws_subnet.public_subnet_1c.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
 }
 
 resource "aws_route_table" "private_rt" {
@@ -111,10 +111,10 @@ resource "aws_route_table" "private_rt" {
 
 resource "aws_route_table_association" "private_rt_1a" {
   route_table_id = aws_route_table.private_rt.id
-  subnet_id = aws_subnet.private_subnet_1a.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
 }
 
 resource "aws_route_table_association" "private_rt_1c" {
   route_table_id = aws_route_table.private_rt.id
-  subnet_id = aws_subnet.private_subnet_1c.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
 }
