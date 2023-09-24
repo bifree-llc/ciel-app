c06533e374734ae46fdbeebe3e4aa46fb128ebf9
bifree <bifree2bfree@gmail.com>
2023-09-25 03:06:44 +0900

    terraform fmt

--- 01_infra/main.tf
+++ 01_infra/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   profile = "terraform"
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
 }
 
 resource "aws_instance" "dev-instance" {
-  ami = "ami-079cd5448deeace01"
+  ami           = "ami-079cd5448deeace01"
   instance_type = "t2.micro"
 }
