db4e6c15f751e7438e42e0926d0ac1df632e82be
bifree <bifree2bfree@gmail.com>
2023-10-13 22:16:26 +0900

    apply css file

--- 02_app/resources/views/app.blade.php
+++ 02_app/resources/views/app.blade.php
@@ -1,26 +1,31 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    @viteReactRefresh
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+  <!-- Scripts -->
+  @viteReactRefresh
+  @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>テストテストテスト</h1>
-    <div id="app">
+  <main>
+    <div class="container">
+      <h1>テストテストテスト</h1>
+      <div class="bold">ようこそ</div>
+      <div id="app">
+      </div>
+      <p>いけた！React x Laravel in Local environmentだ！</p>
     </div>
-    <p>いけた！React x Laravel in Local environmentだ！</p>
+  </main>
 </body>
 </html>
