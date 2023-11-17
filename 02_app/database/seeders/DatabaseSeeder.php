79f45e4de378850d0b8755522fdd6c13b9323721
bifree <bifree2bfree@gmail.com>
2023-11-17 21:51:04 +0900

    delete unused files

--- 02_app/database/seeders/DatabaseSeeder.php
+++ 02_app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Users;
+use App\Models\Posts;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $users = Users::factory()->create();
+        Posts::factory(5)->create([
+            'user_id'=> $users->id
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
